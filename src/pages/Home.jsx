d50bdea876052b043f0d64cae7c14effcf01804f
GreenXDShadow <NickDValen@gmail.com>
2025-11-26 22:38:43 -0800

    Moved css files into CSS folders

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,9 @@ import Navbar from "../components/Navbar";
 import UserForm from "../components/UserForm";
 import { useNavigate } from "react-router-dom";
 import React, { useState, useEffect } from "react";
+import "../css/Home.css";
 
 export default function Home() {
-
 
   const [users, setUsers] = useState([]); //stores list of users
     const [error, setError] = useState(""); //any errors go here
@@ -21,31 +21,16 @@ export default function Home() {
     };
 
   const navigate = useNavigate();
+
   return (
     <>
-      <div
-        style={{
-          minHeight: "100vh",
-          minWidth: "90vw",
-          left: "50%",
-          top: "0%",
-          backgroundImage: `url('https://static.vecteezy.com/system/resources/previews/023/341/569/non_2x/film-reel-on-the-bright-orange-background-vector.jpg')`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          backgroundRepeat: "no-repeat",
-          position: "sticky",
-          zIndex: "-1000",
-          marginTop: "-75vh"
-        }}
-      ></div> {/* sorry ik this is bad lol */}
-      
+      <div className="background-container"></div>
+
       <Navbar />
-      <h1>Welcome to ReelTalk</h1>
-      <h2>For people who want to make a statement</h2>
-      <div
-        className="buttons"
-        style={{ display: "flex", gap: "10px", justifyContent: "center" }}
-      >
+      <h1 className="home-title">Welcome to ReelTalk</h1>
+      <h2 className="home-subtitle">For people who want to make a statement</h2>
+
+      <div className="button-container">
         <button onClick={() => navigate("/moviecatalog")}>
           See available movies
         </button>
@@ -55,11 +40,14 @@ export default function Home() {
           Sign up
         </button>
       </div>
-      <div style={{ height: "300px" }}></div>
+
+      <div className="spacer"></div>
+
       <div id="userForm">
-        <UserForm onSuccess={fetchUsers /* on success, getUsers */} /> 
+        <UserForm onSuccess={fetchUsers /* on success, getUsers */} />
       </div>
-      <div style={{ height: "300px" }}></div>
+
+      <div className="spacer"></div>
     </>
   );
 }
